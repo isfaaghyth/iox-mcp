94dbb0ac03ee765ec79c610c4575fd1d1fea96da
isfaaghyth <isfaaghyth@gmail.com>
2025-07-19 14:28:43 +0700

    feat: add empty state

--- app/src/commonMain/kotlin/app/isfa/iox/domain/GetExpenseListUseCase.kt
+++ app/src/commonMain/kotlin/app/isfa/iox/domain/GetExpenseListUseCase.kt
@@ -1,5 +1,6 @@
 package app.isfa.iox.domain
 
+import app.isfa.iox.data.entity.ExpenseResponse
 import app.isfa.iox.data.repository.ExpenseRepository
 import app.isfa.iox.data.repository.request.ExpenseRequestBody
 import app.isfa.iox.domain.model.ExpenseUiModel
@@ -11,22 +12,21 @@ class GetExpenseListUseCase(private val repository: ExpenseRepository) {
 
     operator fun invoke(): Flow<List<GroupExpenseUiModel>?> {
         return flow {
-            val result = repository.all(fromMcp = false).getOrDefault(emptyList())
+            val result = repository
+                .all(fromMcp = false)
+                .getOrDefault(emptyList())
 
             emit(
-                listOf(
-                    GroupExpenseUiModel(
-                        date = "Hari Ini",
-                        items = result.map {
-                            ExpenseUiModel(
-                                name = it.name,
-                                amount = it.amount.toIntOrNull() ?: 0,
-                                category = it.category,
-                                time = it.time.toLongOrNull() ?: 0
-                            )
-                        }
-                    ),
-                )
+                if (result.isNotEmpty()) {
+                    listOf(
+                        GroupExpenseUiModel(
+                            date = "Hari Ini",
+                            items = result.map(::transformToUiModel)
+                        ),
+                    )
+                } else {
+                    emptyList()
+                }
             )
         }
     }
@@ -34,4 +34,12 @@ class GetExpenseListUseCase(private val repository: ExpenseRepository) {
     suspend fun store(body: ExpenseRequestBody) {
         repository.create(body)
     }
+
+    private fun transformToUiModel(response: ExpenseResponse) =
+        ExpenseUiModel(
+            name = response.name,
+            amount = response.amount.toIntOrNull() ?: 0,
+            category = response.category,
+            time = response.time.toLongOrNull() ?: 0
+        )
 }
